ad2c1bf978d08adb01c3068989d6bdb1812b74ec
Ian Wehrman <iwehrman@gmail.com>
2012-07-11 13:23:02 -0500

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -f dissertation.aux dissertation.bbl dissertation.blg dissertation.toc dissertation.out dissertation.lof dissertation.fdb_latexmk dissertation.log dissertation.synctex.gz
